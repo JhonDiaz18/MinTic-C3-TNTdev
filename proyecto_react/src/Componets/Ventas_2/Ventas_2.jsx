62f8aeec51783ed23b7f3fdee8b4e7b0d77ede85
Jhon <jhon_0621@hotmail.com>
2021-10-04 15:41:50 -0500

    Update Ventas_2

--- proyecto_react/src/Componets/Ventas_2/Ventas_2.jsx
+++ proyecto_react/src/Componets/Ventas_2/Ventas_2.jsx
@@ -1,10 +1,56 @@
 import './Ventas_2.css';
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
 
 const Ventas_2 = () => {
-    return (  
-        <h1>Pagina Ventas_2</h1>
+    return (
+        <>
+            <h1>Pagina Ventas_2</h1>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Cliente</th>
+                        <th>Responsable</th>
+                        <th>Fecha</th>
+                        <th>Total Venta</th>
+                        <th>Estado</th>
+                        <th>Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2547</td>
+                        <td>Pedro Pablo</td>
+                        <td>Juan Manuel</td>
+                        <td>25/10/2021</td>
+                        <td>$1'000.000</td>
+                        <td>En progreso</td>
+                        <td>Borrar</td>
+                    </tr>
+                    <tr>
+                        <td>2547</td>
+                        <td>Pedro Pablo</td>
+                        <td>Juan Manuel</td>
+                        <td>25/10/2021</td>
+                        <td>$1'000.000</td>
+                        <td>En progreso</td>
+                        <td>Borrar</td>
+                    </tr>
+                    <tr>
+                        <td>2547</td>
+                        <td>Pedro Pablo</td>
+                        <td>Juan Manuel</td>
+                        <td>25/10/2021</td>
+                        <td>$1'000.000</td>
+                        <td>En progreso</td>
+                        <td>Borrar</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
     );
 }
- 
+
 export default Ventas_2;

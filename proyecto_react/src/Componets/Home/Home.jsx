5f44df6edcf0790527881429dd763843a5f3a4c5
Jhon <jhon_0621@hotmail.com>
2021-10-05 17:49:02 -0500

    update home

--- proyecto_react/src/Componets/Home/Home.jsx
+++ proyecto_react/src/Componets/Home/Home.jsx
@@ -1,5 +1,7 @@
 import './Home.css';
 import React from 'react';
+import Logo from "./logo.png";
+import Google from "./logo_google.png";
 
 const Home = () => {
     //Funciones
@@ -8,15 +10,29 @@ const Home = () => {
     return (
         //Codigo HTML
         <>
-            <main>
-                <div className="encabezado">
-                    <img src="src\img\logo.png" alt="Logo" />
-                    <h4 >Iniciar sesion con:</h4>
-                    <button>
-                        <img className="imghome" src="src\img\logo_google.png" alt="LogoGoogle" /> <a href="../Productos_1">Gmail</a>
-                    </button>
-                </div>
-            </main>
+        <body className = "bodyhome">
+        <main>
+        <div className="encabezado">
+            <h4 className = "h4Home">Iniciar sesion</h4>
+            {/* <button className = "butoomGoogle">
+                <img src={Google} alt="logoGoogle" />
+                <a href="Productos1.html">Gmail</a> 
+            </button> */}
+        </div>
+
+        <hr/>
+
+        <div className="cuerpoHome">
+            <center>
+                <h1 id="neon">Bienvenidos a TNTdevs</h1>
+                <img src={Logo} alt="logo" />
+            </center>
+            <p className="parrafo"> 
+                TNTdevs es una aplicación de software que permite realizar el seguimiento de las ventas 
+                de un producto y/o servicio en una empresa.
+            </p> 
+        </div>
+        </main>  </body> 
         </>
         //
     );

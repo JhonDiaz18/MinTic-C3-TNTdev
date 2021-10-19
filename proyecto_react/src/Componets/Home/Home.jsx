3c63bc34a848d1918e987ec2fe44be35a8ae8f30
Laura <laura.lampream@gmail.com>
2021-10-19 16:28:39 -0500

    Cambio de ruta del Home y diseño

--- proyecto_react/src/Componets/Home/Home.jsx
+++ proyecto_react/src/Componets/Home/Home.jsx
@@ -1,7 +1,7 @@
 import './Home.css';
 import React from 'react';
 import GoogleLogin from 'react-google-login';
-// import Logo from "./logo.png";
+import Logo from "./logo.png";
 import axios from 'axios';
 
 
@@ -11,55 +11,47 @@ const Home = () => {
     const responseGoogle = async (resp) => {
         console.log(resp);
         try {
-          //** Debido al async se utiliza el await, el await es para que la ejecución de una
-          // promesa se ejecute de forma asincrona */
-          const { data } = await axios({
-            method: 'POST',
-            url: 'http://localhost:4000/api/auth/google/login',
-            headers: {
-              'Authorization': `Bearer ${resp.tokenId}`
-            }
-          });
-          console.log(data);
-  
+            //** Debido al async se utiliza el await, el await es para que la ejecución de una
+            // promesa se ejecute de forma asincrona */
+            const { data } = await axios({
+                method: 'POST',
+                url: 'http://localhost:4000/api/auth/google/login',
+                headers: {
+                    'Authorization': `Bearer ${resp.tokenId}`
+                }
+            });
+            console.log(data);
+
         } catch (error) {
-          console.log(error.toJSON());
-          console.log(error.response.data);
+            console.log(error.toJSON());
+            console.log(error.response.data);
         }
     }
     return (
         //Codigo HTML
         <>
-            <GoogleLogin
-                clientId='737691121751-1ukdm2td5rrivg53cblsg893ppk6vcfl.apps.googleusercontent.com'
-                buttonText="Iniciar Sesión"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                cookiePolicy={'single_host_origin'}
-            />
-    
-            {/* <body className="bodyhome">
-                <main>
-                    <div className="encabezado">
-                        <h4 className="h4Home">Iniciar sesion</h4>
-                    </div>
-
-                    <hr />
-
-                    <div className="cuerpoHome">
-                        <center>
-                            <h1 id="neon">Bienvenidos a TNTdevs</h1>
-                            <img src={Logo} alt="logo" />
-                        </center>
-                        <p className="parrafo">
-                            TNTdevs es una aplicación de software que permite realizar el seguimiento de las ventas
-                            de un producto y/o servicio en una empresa.
-                        </p>
-                    </div>
-                </main>  
-            </body> */}
+            <body >
+           
+                <center>
+                    <h1 >Bienvenidos a TNTdevs</h1>
+                    <img src={Logo} alt="logo" />
+                </center>
+                <h3>
+                    TNTdevs es una aplicación de software que permite realizar el seguimiento de ventas 
+                    en linea y fisicas de productos tecnológicos.
+                </h3>
+                <center>
+                <GoogleLogin
+                    clientId='737691121751-1ukdm2td5rrivg53cblsg893ppk6vcfl.apps.googleusercontent.com'
+                    buttonText="Iniciar Sesión"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
+            </center>
+        </body>
         </>
-        //
+
     );
 }
 

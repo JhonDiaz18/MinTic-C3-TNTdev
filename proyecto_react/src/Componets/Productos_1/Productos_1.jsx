d0eed1367906709f3e790352c270ea0b12590483
Jhon Diaz <jhon_0621@hotmail.com>
2021-10-21 23:26:41 -0500

    registrar_productos

--- proyecto_react/src/Componets/Productos_1/Productos_1.jsx
+++ proyecto_react/src/Componets/Productos_1/Productos_1.jsx
@@ -1,48 +1,88 @@
 import './Productos_1.css';
-import React from 'react';
+// import React from 'react';
 import Header from '../Encabezado/Header';
+import M from 'materialize-css';
 
-const Productos_1 = () => {
-    //Funciones
+import React, { Component } from 'react';
+
+class Productos_1 extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+        name: '',
+        id_product: '',
+        value_product: '',
+        state_product: 'Disponible'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.addTask = this.addTask.bind(this);
+  }
 
 
-    //
+  addTask(e) {
+      fetch('http://localhost:4000/api/productos', {
+        method: 'POST',
+        body: JSON.stringify(this.state
+        ),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })    
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            M.toast({html: 'Task Updated'});
+            this.setState({name: '', id_product: '', value_product: '', state_product: 'Disponible'});
+        //   this.fetchTasks();
+        })
+        e.preventDefault();
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
     return (
-        //Codigo HTML
-
         <main>
             <Header />
-            
-
             <div id="cuerpo">
                 <div id="regProductos">
                     <center>
                         <h2>Registro de Productos</h2>
                     </center>
-                    <form>
-                        <label>ID de producto</label>
-                        <input type="text" name="ID de producto" id="producto" readonly />
-
+                    <form onSubmit= {this.addTask}>
                         <label>Descripcion</label>
-                        <input type="text" name="Descripcion" id="descripcion" />
-
+                        <div className = "name">
+                        <input type="text" name="name" onChange = {this.handleChange} id="descripcion" value ={this.state.name}/>
+                        </div>
+                        <label>ID de producto</label>
+                        <div className = "id_product">
+                        <input type="text" name="id_product" onChange = {this.handleChange} id="producto" value ={this.state.id_product}/>
+                        </div >
                         <label>Valor Unitario</label>
-                        <input type="text" name="unitvalue" id="unitvalue" />
-
+                        <div className = "value_product">
+                        <input type="text" name="value_product" onChange = {this.handleChange} id="unitvalue" value ={this.state.value_product}/>
+                        </div>
                         <label>Estado</label>
-                        <select name="Estado" id="estado">
-                            <option value="1">Disponible</option>
-                            <option value="0">No disponible</option>
+                        <div className = "state_product">
+                        <select name="state_product" id="estado" onChange = {this.handleChange} value ={this.state.state_product}>
+                            <option value="Disponible">Disponible</option>
+                            <option value="No disponible">No disponible</option>
                         </select>
+                        </div>
+                        <button type="submit" className="buttonRegistrar">Registrar</button>
                     </form>
-                    <center><button className="buttonRegistrar">Registrar</button></center>
-                    
                 </div>
             </div>
         </main>
-
-        //
-    );
+    )
+}
 }
 
 export default Productos_1;
